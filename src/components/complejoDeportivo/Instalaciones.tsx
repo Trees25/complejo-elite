a934e4a23449b44deb30de56f3358854e68523fd
Lauta2217 <lautarogarcia94@gmail.com>
2026-06-29 23:24:14 -0300

    Eliminando extensiones .tsx porque netlify es re trolo

--- src/components/complejoDeportivo/Instalaciones.tsx
+++ src/components/complejoDeportivo/Instalaciones.tsx
@@ -1,5 +1,5 @@
 "use client";
-import CarouselScale from "./CarouselScale.tsx";
+import CarouselScale from "./CarouselScale";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
