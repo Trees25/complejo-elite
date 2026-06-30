6e235a0c3006080d8acd901a5c691a52bcd3cae6
Lauta2217 <lautarogarcia94@gmail.com>
2026-06-29 23:18:23 -0300

    Error boludo

--- src/app/complejo-deportivo/page.tsx
+++ src/app/complejo-deportivo/page.tsx
@@ -1,4 +1,3 @@
-import ComplejoDeportivo from "../../components/complejoDeportivo/index.tsx";
 import Hero from "../../components/complejoDeportivo/Hero.tsx";
 import Instalaciones from "../../components/complejoDeportivo/Instalaciones.tsx";
 export default function ComplejoDeportivooo() {
